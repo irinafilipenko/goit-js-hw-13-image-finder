a94b18b3be391cf853c9e0e4b911857a2b7c63af
irinafilipenko <73819257+irinafilipenko@users.noreply.github.com>
2021-05-19 10:15:45 +0300

    image

--- src/index.js
+++ src/index.js
@@ -1,18 +1,57 @@
 // import { defaults } from 'gh-pages';
 import './sass/main.scss';
+// import debounce from 'lodash.debounce';
+import renderFormTempl from './templates/render-form.hbs';
+import NewsApiService from './js/apiService';
+import galleryTempl from './templates/gallery.hbs';
+import galleryCardTempt from './templates/gallery-card.hbs';
 
-import templates from './templates/render-form.hbs';
-import { refs } from './js/refs.js';
+const body = document.querySelector('body');
+body.insertAdjacentHTML('afterbegin', renderFormTempl());
 
-console.log(templates());
-console.log(refs.inputEl);
+const refs = {
+  searchForm: document.querySelector('.search-form'),
 
-refs.body.innerHTML = templates();
-console.log(refs.inputEl);
+  createGalery: document.querySelector('.gallery'),
 
-const url =
-  'https://pixabay.com/api/?key=21672899-2a5ee6aa4aab0c8363895dd3b&q=yellow+flowers&image_type=photo&per_page=5&page=3';
+  loadMoreBtn: document.querySelector('[data-action="load-more"]'),
+};
 
-fetch(url)
-  .then(r => r.json())
-  .then(console.log);
+const newApiService = new NewsApiService();
+console.dir(newApiService);
+
+refs.searchForm.addEventListener('submit', onSearch);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
+
+function onSearch(e) {
+  e.preventDefault();
+  clearGalleryContainer();
+  newApiService.query = e.currentTarget.elements.query.value;
+  if (newApiService.query === '') {
+    return alert('error');
+  }
+  newApiService.resetPage();
+  newApiService.fetchArticles().then(onMakeGallery);
+
+  refs.loadMoreBtn.classList.remove('is-hidden');
+}
+
+function onLoadMore() {
+  newApiService.fetchArticles().then(onMakeGallery);
+}
+
+function onMakeGallery(hits) {
+  refs.createGalery.insertAdjacentHTML('beforeend', galleryTempl(hits));
+}
+function clearGalleryContainer() {
+  refs.createGalery.innerHTML = ' ';
+}
+
+// function onMakeGalleryCard(hits) {
+
+// }
+
+// newApiService.fetchArticles().then(onMakeGalleryCard);
+
+let imgCardEl = document.querySelector('.images_card');
+console.log(imgCardEl);
--- src/js/refs.js
+++
@@ -1,4 +0,0 @@
-export const refs = {
-  body: document.querySelector('body'),
-  inputEl: document.querySelector('.search-form'),
-};
